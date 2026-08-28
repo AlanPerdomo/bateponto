d10de55fe99909478cce2743e7c1105a61513a3a
alanperdomo <alanperdomo@hotmail.com>
2026-08-28 10:18:55 -0300

    feat: update Prettier configuration and enhance user module tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,8 +32,6 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
 
     AuthModule,
-
-    UsersModule,
   ],
 
   controllers: [AppController],
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './users.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
